6e0d6e8069255ba58b052f27026e31e9dca0cbba
Norsi47 <briggsnorsi@yahoo.com>
2022-07-14 22:49:25 -0400

    Initial commit for using search in sql lite

---
+++ app/src/main/java/onye/Norsi/Model/Friends.java
@@ -0,0 +1,61 @@
+package onye.Norsi.Model;
+
+public class Friends {
+    public int id;
+    public String name, address, email, phone;
+
+    //alt insert to auto gen toString
+    @Override
+    public String toString() {
+        return "Friends{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+//alt ins to get constructor, select none
+    public Friends() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
