f488b38c8812f2a5873377c7597eef5026277fda
Norsi47 <briggsnorsi@yahoo.com>
2022-07-17 22:55:34 -0400

    adding method to get friend by name

--- app/src/main/java/onye/Norsi/DataBase/DataBase.java
+++ app/src/main/java/onye/Norsi/DataBase/DataBase.java
@@ -44,7 +44,7 @@ public class DataBase extends SQLiteAssetHelper {
         //setting query builder to the table name
         sqLiteQueryBuilder.setTables(tableName);
         /*cursor to use the queryBuilder using the table name
-        * in the body read what is in the database, then use the sql array string array list */
+         * in the body read what is in the database, then use the sql array string array list */
         Cursor cursor = sqLiteQueryBuilder.query(sqLiteDatabase, sqlSelect, null, null, null, null, null);
 
         //creating empty array list to show result
@@ -54,10 +54,10 @@ public class DataBase extends SQLiteAssetHelper {
             do {
 
                 /*in this do while loop
-                * first create the Friends class to get its variable names
-                * then set the variable names t match what the cursor will do first
-                * then match it with the columns by their names in sqlSelect
-                * since cursor was set to that in line 48*/
+                 * first create the Friends class to get its variable names
+                 * then set the variable names t match what the cursor will do first
+                 * then match it with the columns by their names in sqlSelect
+                 * since cursor was set to that in line 48*/
 
                 Friends friends = new Friends();
                 friends.setId(cursor.getInt(cursor.getColumnIndex("id")));
@@ -82,7 +82,7 @@ public class DataBase extends SQLiteAssetHelper {
         //need to make sure these match column names in table (can find in Model class as well)
         //setting these names in a string array
         String[] sqlSelect = {
-                 "name"
+                "name"
         };
         //needs to match the table name
         //check db browser sql lite to confirm
@@ -106,10 +106,63 @@ public class DataBase extends SQLiteAssetHelper {
         return friendsListResult;
     }
 
+    //get friend by name
+    //String name in body because we are looking for the name
+    @SuppressLint("Range")
+    public List<Friends> getFriendByName(String name) {
+        SQLiteDatabase sqLiteDatabase = getReadableDatabase();
+        SQLiteQueryBuilder sqLiteQueryBuilder = new SQLiteQueryBuilder();
 
+        //need to make sure these match column names in table (can find in Model class as well)
+        //setting these names in a string array
+        String[] sqlSelect = {
+                "name"
+        };
+        //needs to match the table name
+        //check db browser sql lite to confirm
+        String tableName = "Friends";
+
+        //setting query builder to the table name
+        sqLiteQueryBuilder.setTables(tableName);
+        /*cursor to use the queryBuilder using the table name
+         * in the body read what is in the database, then use the sql array string array list */
+        //query to select(*) all from friends, wher Name LIKE %pattern%
+
+      /*  //to get exact name use this way
+        Cursor cursor = sqLiteQueryBuilder.query(sqLiteDatabase, sqlSelect, "Name = ?", new String[]{name},
+                null, null, null);*/
+
+        Cursor cursor = sqLiteQueryBuilder.query(sqLiteDatabase, sqlSelect, "Name LIKE ?", new String[]{"%" + name + "%"}, null, null, null);
+
+        //creating empty array list to show result
+        List<Friends> friendsListResult = new ArrayList<>();
+
+        if (cursor.moveToFirst()) {
+            do {
+
+                /*in this do while loop
+                 * first create the Friends class to get its variable names
+                 * then set the variable names t match what the cursor will do first
+                 * then match it with the columns by their names in sqlSelect
+                 * since cursor was set to that in line 48*/
+
+                Friends friends = new Friends();
+                friends.setId(cursor.getInt(cursor.getColumnIndex("id")));
+                friends.setName(cursor.getString(cursor.getColumnIndex("name")));
+                friends.setAddress(cursor.getString(cursor.getColumnIndex("address")));
+                friends.setEmail(cursor.getString(cursor.getColumnIndex("email")));
+                friends.setPhone(cursor.getString(cursor.getColumnIndex("Phone")));
+
+                friendsListResult.add(friends);
+            }
+            while (cursor.moveToNext());
+
+        }
+        return friendsListResult;
 
 
     }
+}
 
 
 
